9b542046fd66c4a38e9b76fc5560834fa17590a7
chengrui1024 <a2281138561@foxmail.com>
2019-07-14 14:00:19 +0800

    讲png文件转化为jpg

---
+++ convert_jpg.py
@@ -0,0 +1,31 @@
+from PIL import Image
+import cv2 as cv
+import os
+#输入路径
+PATH='/home/chengrui/PycharmProjects/deeplearning/practice/构建cnn平台/实例/大棚/jpg'
+def PNG_JPG(PngPath):
+    img = cv.imread(PngPath, 0)
+    w, h = img.shape[::-1]
+    infile = PngPath
+    outfile = os.path.splitext(infile)[0] + ".jpg"
+    img = Image.open(infile)
+    img = img.resize((int(w / 2), int(h / 2)), Image.ANTIALIAS)
+    try:
+        if len(img.split()) == 4:
+            # prevent IOError: cannot write mode RGBA as BMP
+            r, g, b, a = img.split()
+            img = Image.merge("RGB", (r, g, b))
+            img.convert('RGB').save(outfile, quality=70)
+            os.remove(PngPath)
+        else:
+            img.convert('RGB').save(outfile, quality=70)
+            os.remove(PngPath)
+        return outfile
+    except Exception as e:
+        print("PNG转换JPG 错误", e)
+
+
+if __name__ == '__main__':
+    for fname in os.listdir(PATH):
+      fpath=os.path.join(PATH,fname)
+      PNG_JPG(fpath)
